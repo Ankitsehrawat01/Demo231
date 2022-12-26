af5df8fbb8aa84ed4991484171b3afb5cd168b7d
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-12-26 16:01:15 +0530

    [Ankit Kumar]: Hello World Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _231GitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
